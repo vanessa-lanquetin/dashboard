5322e2e410768e6ba3a883a488e746a1733c1ffb
vanessa-lanquetin <lanquetin.vanessa@gmail.com>
2024-02-15 16:43:34 +0100

    add class for style for relance date

--- front/src/components/TrackingLineRow.vue
+++ front/src/components/TrackingLineRow.vue
@@ -1,31 +1,31 @@
 <template>
   <template v-if="trackingLine">
-  <tr>
-    <td>
-      {{trackingLine.enterpriseName}}
-    </td>
-    <td>
-      {{date(trackingLine.submissionDate)}}
-    </td>
-    <td>
-      {{getTranslation(trackingLine.type)}}
-    </td>
-    <td>
-      {{trackingLine.jobName}}
-    </td>
-    <td>
-      {{date(trackingLine.relaunchDate)}}
-    </td>
-    <td class="actions">
-      <button @click="$router.push({name: 'tracking-line', params: {trackingLineId: trackingLine.id}})"><i class="fas fa-eye"></i></button>
-      <button  @click="$emit('deleteTrackingLine', trackingLine.id)"><i class="fas fa-trash"></i></button>
-    </td>
-  </tr>
+    <tr :class="{ 'red-background': shouldHighlight(trackingLine.submissionDate, trackingLine.relaunchDate) }">
+      <td>
+        {{ trackingLine.enterpriseName }}
+      </td>
+      <td>
+        {{ date(trackingLine.submissionDate) }}
+      </td>
+      <td>
+        {{ getTranslation(trackingLine.type) }}
+      </td>
+      <td>
+        {{ trackingLine.jobName }}
+      </td>
+      <td>
+        {{ date(trackingLine.relaunchDate) }}
+      </td>
+      <td class="actions">
+        <button @click="$router.push({ name: 'tracking-line', params: { trackingLineId: trackingLine.id } })"><i class="fas fa-eye"></i></button>
+        <button @click="$emit('deleteTrackingLine', trackingLine.id)"><i class="fas fa-trash"></i></button>
+      </td>
+    </tr>
   </template>
 </template>
 
 <script setup>
-import {date, getTranslation} from '../helpers/helper'
+import { date, getTranslation } from '../helpers/helper'
 
 defineProps({
   trackingLine: {
@@ -35,8 +35,16 @@ defineProps({
 })
 defineEmits(['deleteTrackingLine'])
 
+const shouldHighlight = (submissionDate, relaunchDate) => {
+  if (!submissionDate || !relaunchDate) return false;
+  const sixDaysInMilliseconds = 6 * 24 * 60 * 60 * 1000; // 6 days in milliseconds
+  return new Date(relaunchDate) - new Date(submissionDate) >= sixDaysInMilliseconds;
+};
 </script>
 
 <style lang="scss" scoped>
-
+.red-background {
+  background-color: #d36363;
+  border: 2px solid red;
+}
 </style>
