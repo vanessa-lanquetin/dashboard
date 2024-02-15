52d61a1bb5670b8b16522364c16ecb6f63de6157
vanessa-lanquetin <lanquetin.vanessa@gmail.com>
2024-02-15 16:49:27 +0100

    add comments

--- front/src/components/TrackingLineRow.vue
+++ front/src/components/TrackingLineRow.vue
@@ -1,5 +1,7 @@
 <template>
+  <!-- Vérifie si une ligne de suivi est présente -->
   <template v-if="trackingLine">
+    <!-- Ajoute la classe 'red-background' si la condition de mise en surbrillance est vraie -->
     <tr :class="{ 'red-background': shouldHighlight(trackingLine.submissionDate, trackingLine.relaunchDate) }">
       <td>
         {{ trackingLine.enterpriseName }}
@@ -17,7 +19,9 @@
         {{ date(trackingLine.relaunchDate) }}
       </td>
       <td class="actions">
+        <!-- Bouton pour voir les détails de la ligne de suivi -->
         <button @click="$router.push({ name: 'tracking-line', params: { trackingLineId: trackingLine.id } })"><i class="fas fa-eye"></i></button>
+        <!-- Bouton pour supprimer la ligne de suivi -->
         <button @click="$emit('deleteTrackingLine', trackingLine.id)"><i class="fas fa-trash"></i></button>
       </td>
     </tr>
@@ -27,22 +31,27 @@
 <script setup>
 import { date, getTranslation } from '../helpers/helper'
 
+// Définit les propriétés
 defineProps({
   trackingLine: {
     /** @type {import ('../models/TrackingLine').default | null | undefined} */
     default: null
   }
 })
+
+// Définit les événements émis
 defineEmits(['deleteTrackingLine'])
 
+// Fonction pour déterminer si la ligne de suivi doit être mise en surbrillance
 const shouldHighlight = (submissionDate, relaunchDate) => {
   if (!submissionDate || !relaunchDate) return false;
-  const sixDaysInMilliseconds = 6 * 24 * 60 * 60 * 1000; // 6 days in milliseconds
+  const sixDaysInMilliseconds = 6 * 24 * 60 * 60 * 1000; // 6 jours en millisecondes
   return new Date(relaunchDate) - new Date(submissionDate) >= sixDaysInMilliseconds;
 };
 </script>
 
 <style lang="scss" scoped>
+/* Style pour mettre en surbrillance la ligne de suivi */
 .red-background {
   background-color: #d36363;
   border: 2px solid red;
